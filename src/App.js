daa4058feb8159c60734bb05e56d684a00fbaa65
jithin <jithin.reddy3m@gmail.com>
2024-07-21 16:38:58 -0500

    initial commit
    Co-authored-by: Saran Kumar Allam <saran1209@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import SignupForm from './signup/SignupForm';
+import LoginForm from './login/LoginForm.js';
+import HomeForm from './home/HomeForm.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          index 
+          element = {<HomeForm/>}
+        />
+        <Route
+          exact path = '/home'
+          element = {<HomeForm/>}
+        />
+        <Route
+          exact path = '/login'
+          element = {<LoginForm/>}
+        />
+        <Route
+          exact path = "/signup"
+          element = {<SignupForm/>}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/login/LoginForm.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function LoginForm() {
+    const [loginUserName, setLoginUsername] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+
+    const loginSubmit = (login) => {
+        login.preventDefault();
+        console.log('LoginuserName is ' + loginUserName);
+        console.log('loginPassword is ' + loginPassword);
+    };
+
+    return(
+        <div class = "LoginForm">
+            <h1> Login Form</h1>
+            <form onSubmit={loginSubmit}>
+                <div>
+                    <label> UserName </label>
+                    <input 
+                        type ="text" 
+                        value = {loginUserName}
+                        placeholder = "Enter your username" 
+                        onChange = { (loginUserNameValue) => setLoginUsername(loginUserNameValue.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label> Password </label>
+                    <input 
+                        type ="text" 
+                        value = {loginPassword}
+                        placeholder = "Enter your username" 
+                        onChange = { (loginPasswordValue) => setLoginPassword(loginPasswordValue.target.value)}
+                        required
+                    />    
+                </div>
+                <button type="submit">Submit</button>
+                <div>
+                    <h6> not logged in user ?</h6>
+                    {/* <Link to="./signup/signupForm.js" style={{color: 'white'}}> signup </Link> */}
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
